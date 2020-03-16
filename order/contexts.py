3559e998d3f5839831d009a7ede51e9ab184fdf0
rayomeara <rayomeara@gmail.com>
2020-03-16 18:00:18 +0000

    Work on share and commodity order items for pages

--- order/contexts.py
+++ order/contexts.py
@@ -1,19 +1,31 @@
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from listing.models import Share, Commodity
 
 
 def order_contents(request):
     """ Ensures that the order contents are available when rendering every page """
+    share_order = process_order(request, 'share_order')
+    commodity_order = process_order(request, 'commodity_order')
+    total = share_order[1] + commodity_order[1]
+    product_count = share_order[2] + commodity_order[2]
 
-    order = request.session.get('order', {})
+    return {'share_order_items': share_order[0],
+            'commodity_order_items': commodity_order[0],
+            'total': total, 'product_count': product_count}
 
+
+def process_order(request, order_type):
+    order = request.session.get(order_type, {})
     order_items = []
     total = 0
     product_count = 0
     for id, quantity in order.items():
-        product = get_object_or_404(Product, pk=id)
-        total += quantity * product.price
+        if order_type == 'share_order':
+            item = get_object_or_404(Share, pk=id)
+        else:
+            item = get_object_or_404(Commodity, pk=id)
+        total += quantity * item.price
         product_count += quantity
-        order_items.append({'id': id, 'quantity': quantity, 'product': product})
+        order_items.append({'id': id, 'quantity': quantity, 'item': item})
 
-    return {'order_items': order_items, 'total': total, 'product_count': product_count}
+    return order_items, total, product_count
--- order/views.py
+++ order/views.py
@@ -6,29 +6,45 @@ def view_order(request):
     return render(request, "order.html")
 
 
-def add_to_order(request, id):
+def add_to_share_order(request, id):
+    return add_to_order('share_order', request, id)
+
+
+def add_to_commodity_order(request, id):
+    return add_to_order('commodity_order', request, id)
+
+
+def add_to_order(order_type, request, id):
     """ Add a quantity of the specified product to the order """
     quantity = int(request.POST.get('quantity'))
 
-    order = request.session.get('order', {})
+    order = request.session.get(order_type, {})
     if id in order:
         order[id] = int(order[id]) + quantity
     else:
         order[id] = order.get(id, quantity)
 
-    request.session['order'] = order
+    request.session[order_type] = order
     return redirect(reverse('index'))
 
 
-def adjust_order(request, id):
+def adjust_commodity_order(request, id):
+    return adjust_order('commodity_order', request, id)
+
+
+def adjust_share_order(request, id):
+    return adjust_order('share_order', request, id)
+
+
+def adjust_order(order_type, request, id):
     """ Adjust the quantity of the specified product to the specified amount """
     quantity = int(request.POST.get('quantity'))
-    order = request.session.get('order', {})
+    order = request.session.get(order_type, {})
 
     if quantity > 0:
         order[id] = quantity
     else:
         order.pop(id)
 
-    request.session['order'] = order
+    request.session[order_type] = order
     return redirect(reverse('view_order'))
