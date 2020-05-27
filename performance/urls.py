ada1fd26cade7275890240370ad6806ef7797037
rayomeara <rayomeara@gmail.com>
2020-05-27 18:19:07 +0000

    Initial work with Django-Chartjs

--- performance/urls.py
+++ performance/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import show_performance
+from .views import show_performance, line_chart_json
 
 urlpatterns = [
     url(r'^$', show_performance, name='show_performance'),
+    url('chartJSON', line_chart_json, name='line_chart_json'),
 ]
--- performance/views.py
+++ performance/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from listing.models import Share, Commodity
+from chartjs.views.lines import BaseLineChartView
 
 
 def show_performance(request):
@@ -7,3 +8,23 @@ def show_performance(request):
     shares = Share.objects.all()
     commodities = Commodity.objects.all()
     return render(request, "performance.html", {"shares": shares, "commodities": commodities})
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart_json = LineChartJSONView.as_view()
