80fd0c75b5c8c2880bedd8f55a569b76820db258
rayomeara <rayomeara@gmail.com>
2020-05-29 14:47:21 +0000

    Managed to get chart talking to python backend

--- performance/views.py
+++ performance/views.py
@@ -11,20 +11,32 @@ def show_performance(request):
 
 
 class LineChartJSONView(BaseLineChartView):
+
+    providers = ["Central", "Eastside", "Westside"]
+    labels = ["January", "February", "March", "April", "May", "June", "July"]
+    data = [[75, 44, 92, 11, 44, 95, 35], [41, 92, 18, 3, 73, 87, 92], [87, 21, 94, 3, 90, 13, 65]]
+
     def get_labels(self):
         """Return 7 labels for the x-axis."""
-        return ["January", "February", "March", "April", "May", "June", "July"]
+        return self.labels
 
     def get_providers(self):
         """Return names of datasets."""
-        return ["Central", "Eastside", "Westside"]
+        return self.providers
+
+    def post(self, request):
+        post_request = request.POST
+        self.name = post_request['name']
+
+        self.providers = ["A", "B", "C"]
+        context = self.get_context_data()
+        print('post context data: ', context)
+
+        return self.render_to_response(context)
 
     def get_data(self):
         """Return 3 datasets to plot."""
-
-        return [[75, 44, 92, 11, 44, 95, 35],
-                [41, 92, 18, 3, 73, 87, 92],
-                [87, 21, 94, 3, 90, 13, 65]]
+        return self.data
 
 
 line_chart_json = LineChartJSONView.as_view()
