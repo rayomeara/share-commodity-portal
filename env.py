723db152f3f26677b225f94f2fcd14e6389db200
rayomeara <rayomeara@gmail.com>
2020-06-18 17:49:15 +0000

    removed env.py from github

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_feaKQYSiBFLVsicidwIZ1Ws3")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_fewBMIFwMTTmNZVaiNrI7PgF")
-os.environ.setdefault("DATABASE_URL", "postgres://yyjuohkytisrqr:99f9ceea169c5aef2a73edf37961078c8fae49685a0bf1930f560890e279fcb4@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dcua9072go8e4l")
-os.environ.setdefault("SECRET_KEY", "fs1q+kbf0unyzwp9e$_o5&zqsqe8u4ys61(*9bovi7jbt!z@wt")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIA4MRWCHBZGJ7FDXGH")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
