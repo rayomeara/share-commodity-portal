f5a25ecbaedea3a4524ba33ba1ce9e4d7f784df5
rayomeara <rayomeara@gmail.com>
2020-06-22 12:38:38 +0000

    Altered wallet credit amount

--- broker_portal/settings.py
+++ broker_portal/settings.py
@@ -149,7 +149,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
